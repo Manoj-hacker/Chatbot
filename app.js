347d9e2f29ef837127ab35b893d412f645bf9dd5
VASEEKARAN S <vaseekaransaminathan@gmail.com>
2022-12-05 19:31:13 +0530

    initial

---
+++ app.js
@@ -0,0 +1,20 @@
+function talk(){
+    var know = {
+    "Who are you" : "Hello, Codewith_random here ",
+    "Hello":"hi",
+    "Are you single":"No",
+    "I love You" : "Love you too ",
+    "How are you" : "Good :)",
+    "What can i do for you" : "Please Give us A Follow & Like.",
+    "Your followers" : "I have my family of 5000 members, i don't have follower ,have supportive Famiy ",
+    "ok" : "Thank You So Much ",
+    "Bye" : "Okay! Will meet soon.."
+    };
+    var user = document.getElementById('userBox').value;
+    document.getElementById('chatLog').innerHTML = user + "<br>";
+    if (user in know) {
+    document.getElementById('chatLog').innerHTML = know[user] + "<br>";
+    }else{
+    document.getElementById('chatLog').innerHTML = "Sorry,I didn't understand <br>";
+    }
+    }
